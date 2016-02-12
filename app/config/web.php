cfe6f88f8de01639b8f73641908a97ae2492bcae
boehsermoe <boehsermoe@me.com>
2016-02-12 08:02:00 +0100

    Url rule for admin panel

--- app/config/web.php
+++ app/config/web.php
@@ -55,6 +55,7 @@ $config = [
                 //'<controller:\w+>/<id:\d+>' => '<controller>/view',
                 //'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 //'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                'admin' => 'admin',
                 '<id:[\d\w]+>' => 'site/content',
 	        ]
         ],
